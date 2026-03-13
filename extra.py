86f7adde6da4574cc10a5cf4fb3d2acf293fbbd0
Gub Radnev <gubst111@gmail.com>
2026-03-13 18:08:36 +0200

    Add files via upload

--- extra.py
+++ extra.py
@@ -1,16 +1,67 @@
 import random
-correct_number = random.randint(1, 100)
-while True:
+correct_number_level_1 = random.randint(1, 100)
+correct_number_level_2 = random.randint(1, 200)
+correct_number_level_3 = random.randint(1, 300)
+attempts = 0
+level = 1
+while level == 1:
     player_guess = input("Guess the number (1-100): ")
     if not player_guess.isdigit():
         print("Invalid input. Try again...")
         continue
-    player_guess = int(player_guess)
-    if player_guess == correct_number:
-        print("You guessed it!")
+    if attempts >= 10:
+        print("You lose! Please restart the game!")
         break
-    if player_guess > correct_number:
+    player_guess = int(player_guess)
+    if player_guess == correct_number_level_1:
+        print("You guessed it!")
+        level += 1
+        attempts = 0
+        break
+    if player_guess > correct_number_level_1:
         print("Too High!")
         continue
     else:
         print("Too Low!")
+    attempts += 1
+
+while level == 2:
+    player_guess = input("Guess the number (1-200): ")
+    if not player_guess.isdigit():
+        print("Invalid input. Try again...")
+        continue
+    if attempts >= 10:
+        print("You lose! Please restart the game!")
+        break
+    player_guess = int(player_guess)
+    if player_guess == correct_number_level_2:
+        print("You guessed it!")
+        level += 1
+        attempts = 0
+        break
+    if player_guess > correct_number_level_2:
+        print("Too High!")
+        continue
+    else:
+        print("Too Low!")
+    attempts += 1
+
+while level == 3:
+    player_guess = input("Guess the number (1-300): ")
+    if not player_guess.isdigit():
+        print("Invalid input. Try again...")
+        continue
+    if attempts >= 10:
+        print("You lose! Please restart the game!")
+        break
+    player_guess = int(player_guess)
+    if player_guess == correct_number_level_3:
+        print("You guessed it!")
+        level += 1
+        attempts = 0
+        break
+    if player_guess > correct_number_level_3:
+        print("Too High!")
+    else:
+        print("Too Low!")
+    attempts += 1
